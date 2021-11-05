ebe5c30c0ca68a55a5eedcb66bc92db259d2b816
SHshzik <andrycalnicow@gmail.com>
2021-11-05 23:17:14 +0400

    add hero unit

---
+++ units/empire/hero/knight.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require './units/unit'
+
+# The Knight represent Knight hero unit
+class Knight < Unit
+  def full_hp
+    150
+  end
+
+  def dmg
+    50
+  end
+end
