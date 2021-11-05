1035b6c19d82d1c4c4b07c18f54b21c555d61f09
SHshzik <andrycalnicow@gmail.com>
2021-11-05 23:07:22 +0400

    add squire class

---
+++ units/squire.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require './units/unit'
+
+# The Squire represents squire unit
+class Squire < Unit
+  def full_hp
+    100
+  end
+
+  def dmg
+    25
+  end
+
+  def attack_type
+    1
+  end
+end
