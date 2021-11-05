852a3e03c98899f4ad59bb9f0f55001c04abe865
SHshzik <andrycalnicow@gmail.com>
2021-11-05 23:38:48 +0400

    add fight class

---
+++ fight.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# The Fight class save info about teams and some data
+class Fight
+  attr_reader :friend_team, :enemy_team
+
+  def initialize(friend_team, enemy_team)
+    @friend_team = friend_team
+    @enemy_team = enemy_team
+  end
+
+  def initialize_copy(original_fight)
+    @friend_team = original_fight.friend_team.dup
+    @enemy_team = original_fight.enemy_team.dup
+  end
+end
